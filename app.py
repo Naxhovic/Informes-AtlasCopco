43b91364709b3beba69278afe715b3f837870b00
Ignacio Morales <ignacio.a.morales@atlascopco.com>
2026-03-02 17:06:30 -0300

    Actualización a OneDrive y mejoras de texto

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ inventario_equipos = {
     "50-GC-001": ["GA 45", "API542705", "planta SX", "área húmeda"], "50-GC-002": ["GA 45", "API542706", "planta SX", "área húmeda"], "50-GC-003": ["ZT 37", "API791692", "planta SX", "área húmeda"], "50-GC-004": ["ZT 37", "API791693", "planta SX", "área húmeda"], "50-CD-001": ["CD 80+", "API095825", "planta SX", "área húmeda"], "50-CD-002": ["CD 80+", "API095826", "planta SX", "área húmeda"],
     "55-GC-015": ["GA 30", "API501440", "planta borra", "área húmeda"],
     "65-GC-009": ["GA 250", "APF253608", "patio de estanques", "área húmeda"], "65-GC-011": ["GA 250", "APF253581", "patio de estanques", "área húmeda"], "65-CD-011": ["CD 630", "WXF300015", "patio de estanques", "área húmeda"], "65-CD-012": ["CD 630", "WXF300016", "patio de estanques", "área húmeda"],
-    "70-GC-013": ["GA 132", "AIF095296", "descarga de acido", "área húmeda"], "70-GC-014": ["GA 132", "AIF095297", "descarga de acido", "área húmeda"],
+    "70-GC-013": ["GA 132", "AIF095296", "descarga de ácido", "área húmeda"], "70-GC-014": ["GA 132", "AIF095297", "descarga de ácido", "área húmeda"],
     "Taller": ["GA 18", "API335343", "Laboratorio", "Taller"]
 }
 
